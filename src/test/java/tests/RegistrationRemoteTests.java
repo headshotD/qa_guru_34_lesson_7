f278f071acf1e21fbbee838222575d21253001f7
Oleg <namozov@dev.vtb.ru>
2025-04-13 23:05:30 +0300

    added properties

--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -27,7 +27,7 @@ public class RegistrationRemoteTests {
     @BeforeAll
     static void beforeAll() {
         baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
         Configuration.timeout = 5000;
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
